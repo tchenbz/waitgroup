b796a879b4b10674590b68b7243c6bee56328176
tchenbz <2019120211@ub.edu.bz>
2023-02-15 13:08:57 -0600

    added uppercase flag

--- main.go
+++ main.go
@@ -13,9 +13,13 @@ func main() {
 	//set flags
 	msg := flag.String("msg", "Howdy, stranger!", "the message to display")
 	num := flag.Int("num", 1, "how many times to print the message")
-	caps := flag.Bool("caps", false, "should the string be all caps")
+	caps := flag.Bool("U", false, "specify whether to uppercase the string (true or false)")
 	flag.Parse()
-	
+	//check if it should be uppercase before printing it
+	if *caps {
+		*msg = strings.ToUpper(*msg)
+	}
+	//print the string
 	for i := 0; i < *num; i++ {
 		fmt.Println(*msg)
 	}
